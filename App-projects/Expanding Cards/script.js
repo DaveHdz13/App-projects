9cd0f1e70abe048baacd3ce0157769d5cdec9342
DaveHdz <76536169+DaveHdz13@users.noreply.github.com>
2023-09-29 15:21:07 -0600

    Expanding cards
    - Flex and transition

---
+++ App-projects/Expanding Cards/script.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll(".card")
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    removeClass()
+    card.classList.add("active")
+  })
+});
+
+const removeClass = () => {
+  cards.forEach(card => {
+    card.classList.remove("active")
+  })
+}
